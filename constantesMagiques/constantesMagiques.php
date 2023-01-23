c3a1162ef30540e400b8a41ea11624692819d8f1
david <d.minatchy@outlook.com>
2023-01-23 21:55:31 +0100

    Create constantesMagiques.php

---
+++ constantesMagiques/constantesMagiques.php
@@ -0,0 +1,14 @@
+<?php
+
+class Perso {
+    public static function infosPerso(){
+      return static::quiSuisje();
+    }
+  
+    public static function quiSuisje()
+    {
+          return "je suis perso";
+    }
+  }
+  
+?>
